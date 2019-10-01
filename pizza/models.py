401e9cafce1351f905135714864c48ae9707bce5
Yoimer <ydrf19@gmail.com>
2019-10-01 18:27:06 -0430

    fixed bug on models.py. it shows nows title values order page

--- pizza/models.py
+++ pizza/models.py
@@ -4,14 +4,15 @@ from django.db import models
 
 class Size(models.Model):
     title = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
 
 # this will help us show the size in forms and in the adming panel.
 # __str__: works for any class NOT only classes in django
 # and what it does is define what the object should look like
 # when it's printed out to a screen. It applies when it's printed
 # to a terminal or to a HTML.
-def __str__(self):
-    return self.title
+
 
 class Pizza(models.Model):
     topping1 = models.CharField(max_length=100)
