b5875efc4c24b769de9a7546d793eae97fc6f89f
Yoimer <ydrf19@gmail.com>
2019-10-10 19:33:12 -0430

    created scripts folder and apps.js script. this is meant to avoid error when submitting all empty fields on pizzas.html

---
+++ static/scripts/apps.js
@@ -0,0 +1,26 @@
+//this code checks whether all toppings and sizes fields are empty. if so prompt warning
+
+// number of pizzas from Get Pizzas button  on order.html
+let number_of_pizzas = document.getElementById("id_form-TOTAL_FORMS").value;
+
+// pizza elements
+let pizza_elements = ["topping1", "topping2", "size"];
+
+// each pizza array. this will save each field ElementById
+var each_pizza_array = [];
+
+for (let i = 0; i < number_of_pizzas ; i++) {
+    pizza_elements.forEach(function(item) {
+        let each_pizza = "id_form-" + i + "-" + item;
+        each_pizza_array.push(document.getElementById(each_pizza).value);
+    });
+}
+
+console.log(each_pizza_array);
+
+//if all fields are empty, returns true.
+function isEmpty(topping_and_size) {
+  return topping_and_size == "";
+}
+
+console.log(each_pizza_array.every(isEmpty));
