1ba5ee03675e45b0fdcdb62c0177819b60fa3266
Yoimer <ydrf19@gmail.com>
2019-10-15 19:45:11 -0430

    added EmptyFieldController on app.js. removed script tags on pizzas.html file

--- pizza/static/scripts/apps.js
+++ pizza/static/scripts/apps.js
@@ -1,26 +1,37 @@
-//this code checks whether all toppings and sizes fields are empty. if so prompt warning
+var EmptyFieldController = (function() {
 
-// number of pizzas from Get Pizzas button  on order.html
-let number_of_pizzas = document.getElementById("id_form-TOTAL_FORMS").value;
+    document.getElementById("multiplepizzaform").addEventListener("submit", proccess_multiple_pizzas);
+    function proccess_multiple_pizzas() {
+        //this code checks whether all toppings and sizes fields are empty. if so, prompts warning
 
-// pizza elements
-let pizza_elements = ["topping1", "topping2", "size"];
+        // number of pizzas from Get Pizzas button  on order.html
+        let number_of_pizzas = document.getElementById("id_form-TOTAL_FORMS").value;
 
-// each pizza array. this will save each field ElementById
-var each_pizza_array = [];
+        // pizza elements
+        let pizza_elements = ["topping1", "topping2", "size"];
 
-for (let i = 0; i < number_of_pizzas ; i++) {
-    pizza_elements.forEach(function(item) {
-        let each_pizza = "id_form-" + i + "-" + item;
-        each_pizza_array.push(document.getElementById(each_pizza).value);
-    });
-}
+        // each pizza array. this will save each field ElementById
+        var each_pizza_array = [];
 
-console.log(each_pizza_array);
+        for (let i = 0; i < number_of_pizzas ; i++) {
+            pizza_elements.forEach(function(item) {
+                let each_pizza = "id_form-" + i + "-" + item;
+                each_pizza_array.push(document.getElementById(each_pizza).value);
+            });
+        }
 
-//if all fields are empty, returns true.
-function isEmpty(topping_and_size) {
-  return topping_and_size == "";
-}
+        console.log(each_pizza_array);
 
-console.log(each_pizza_array.every(isEmpty));
+        //if all fields are empty, returns true.
+        function isEmpty(topping_and_size) {
+            return topping_and_size == "";
+        }
+
+        console.log(each_pizza_array.every(isEmpty));
+
+        if(each_pizza_array.every(isEmpty)) {
+            alert("Fields are Empty. Please, fill them all");
+        }
+    }
+
+})();
