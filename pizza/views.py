3d0487bd296d513fd934d936ffdcdaf52612c918
Yoimer <ydrf19@gmail.com>
2019-11-05 21:25:39 -0430

    added piece of code that updates values on db when editing multi order

--- pizza/views.py
+++ pizza/views.py
@@ -72,7 +72,7 @@ def pizzas(request):
         if(created_pizza_pk == " "):
             return render(request, 'pizza/pizzas.html', {'note':note, 'formset':formset})
         else:
-            return render(request, 'pizza/pizzas.html', {'created_pizza_pk':created_pizza_pk, 'note':note, 'formset':formset, 'nop': number_of_forms-1})
+            return render(request, 'pizza/pizzas.html', {'created_pizza_pk':created_pizza_pk, 'note':note, 'formset':formset, 'nop': number_of_forms})
     else:
         return render(request, 'pizza/pizzas.html', {'formset':formset})
 
@@ -92,7 +92,6 @@ def edit_order(request, pk):
 def edit_multi_order(request, pk, nop):
 
     pizza = Pizza.objects.get(pk=pk)
-    form = PizzaForm(instance=pizza)
     note = 'editing_multi_order.'
 
     # creates as many forms as number of pizzas has (nop stands for number of pizzas)
@@ -101,11 +100,26 @@ def edit_multi_order(request, pk, nop):
 
     # checks values saved on db and retrieve them on forms
     j = 0
-    for i in range((pk-nop), pk):
-        temp_from_db = Pizza.objects.get(pk=i)
-        formset.forms[j].initial.update({'size': temp_from_db.size})
-        formset.forms[j].initial.update({'topping1': temp_from_db.topping1})
-        formset.forms[j].initial.update({'topping2': temp_from_db.topping2})
+
+    # updates values from fields and saves them on db
+    for i in range((pk - nop) + 1, (pk + 1)):
+        pizza_from_db = Pizza.objects.get(pk=i)
+        formset.forms[j].initial.update({'size': pizza_from_db.size})
+        formset.forms[j].initial.update({'topping1': pizza_from_db.topping1})
+        formset.forms[j].initial.update({'topping2': pizza_from_db.topping2})
+
+        post_form_values = {}
+        to_db = {}
+        post_form_values = request.POST
+
+        if request.method == 'POST':
+            to_db['size'] = post_form_values['form-' + str(j) + '-' + 'size']
+            to_db['topping1'] = post_form_values['form-' + str(j) + '-' + 'topping1']
+            to_db['topping2'] = post_form_values['form-' + str(j) + '-' + 'topping2']
+            filled_form = PizzaForm(to_db, instance=pizza_from_db)
+            if filled_form.is_valid():
+                filled_form.save()
+
         j = j + 1
 
-    return render(request, 'pizza/edit_multi_order.html', {'note':note,'pizzaform':form, 'pizza':pizza, 'formset':formset, 'nop':nop})
+    return render(request, 'pizza/edit_multi_order.html', {'note':note,'pizza':pizza, 'formset':formset, 'nop':nop})
